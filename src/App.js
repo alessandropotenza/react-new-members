75d641ef919121760827be9fbec6586ef80a3ef2
Ethan Alexander <ethan315.ja@gmail.com>
2023-05-07 13:00:50 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+
+// import styled from 'styled-components';
+
+//look at state variable to change text on label
+
 
 function App() {
+
+  const [num, setNum] = useState(0);
+
   return (
-    <h1>This is the template!</h1>
+    <div className='outer' style={{backgroundColor: "white"}}>
+      <button id="but1" type="button">-</button>
+      <div className="inner">
+       <button onClick={setNum(num+1)}>
+        +
+       </button>
+      </div>
+      <label id="l1" type="label">{num}</label>
+    </div>
   );
 }
 
