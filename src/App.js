e07fe7e15f32c6fc09473740817014ec86dfd9a4
Ozone <owenzonneveld@gmail.com>
2023-10-04 18:48:50 -0500

    Branch with examples and some CSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import './App.css';
 
+import React, { useState } from 'react';
+
+
 function App() {
+  const [value, setValue] = useState(2);
+  const [text, setText] = useState("First");
+
+  function multiply() {
+    setValue(value * 2)
+  }
+
+  function divide() {
+    setValue(value / 2)
+  }
+
+  function change_text() {
+    if (text == "First") {
+      setText("Second");
+    }
+    else if (text == "Second") {
+      setText("Third");
+    }
+    else if (text == "Third") {
+      setText("First");
+    }
+  }
+
   return (
-    <h1>This is the template!</h1>
+    <body>
+      <div className="centering">
+        <center >
+        <p className="numberdisplay">{value}</p>
+        <button className="bluebutton" onClick={() => multiply()}> X </button>
+        <button className="bluebutton" onClick={() => divide()}> / </button>
+        <br></br>
+        <button className="textbutton" onClick={() => change_text()}> {text} </button>
+        </center>
+      </div>
+    </body>
   );
 }
 
